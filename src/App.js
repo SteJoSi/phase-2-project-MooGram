d90f2d119d57057114c6d8b34d39cbb808b86c65
Stephanie Sison <sjsison27@icloud.com>
2022-09-26 22:25:53 -0700

    data made for db.json

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header>MooGram</header>
+      {/* <img src="https://img.buzzfeed.com/buzzfeed-static/static/2017-10/19/14/asset/buzzfeed-prod-fastlane-03/sub-buzz-2288-1508439279-2.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto" alt='cow'/> */}
     </div>
   );
 }
