e71be8b53bdf7111e35c159dfb158b961fa8ceed
Stephanie Sison <sjsison27@icloud.com>
2022-09-27 20:47:32 -0700

    Create Login form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from "react";
 // import ReactDOM  from "react";
 import {Route} from "react-router-dom";
 
+//this will display data/pictures of MooMoo's 
 function Home() {
   return (
-    <div>MooGram!</div>
+    <div>
+      <header>MooGram!</header>
+    </div>
+  );
+}
+
+//this will display login page for user to access their profile
+function Login() {
+  return (
+    <div>
+      <h1>Login here!</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   );
 }
 
