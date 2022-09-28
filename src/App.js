0ecc82833511d83f8b86e948470381ae37e94385
Stephanie Sison <sjsison27@icloud.com>
2022-09-27 20:43:15 -0700

    create Home function and a Home Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+// import ReactDOM  from "react";
+import {Route} from "react-router-dom";
+
+function Home() {
+  return (
+    <div>MooGram!</div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header>MooGram</header>
-      {/* <img src="https://img.buzzfeed.com/buzzfeed-static/static/2017-10/19/14/asset/buzzfeed-prod-fastlane-03/sub-buzz-2288-1508439279-2.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto" alt='cow'/> */}
-    </div>
+    <Route path="/">
+      <Home />
+    </Route>
   );
 }
 
