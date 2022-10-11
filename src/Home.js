cc20b8276544f0aea7fb41adaf5248f2e2b2e707
Stephanie Sison <sjsison27@icloud.com>
2022-10-11 10:00:50 -0700

    attempt to run db.json

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //this will display collage pictures of MooMoo's and navbar 
 function Home() {
+    // const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/cows")
+        .then(resp => resp.json())
+        .then((data) => console.log("json", data))
+    }, []);
+
     return (
         <div id="title">
             <header>MooGram!</header>
         </div>
-        
+
     );
 }
 
