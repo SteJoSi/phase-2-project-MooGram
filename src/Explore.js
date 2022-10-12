1b3b38a5b464b2e6501cb3298c72f8b1b02e038c
Stephanie Sison <sjsison27@icloud.com>
2022-10-12 13:16:05 -0700

    create GET request and import data from db.json

--- src/Explore.js
+++ src/Explore.js
@@ -5,6 +5,7 @@ function Explore() {
     return (
         <div>
             <h1>Explore Cows Here!</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvmfjg9mxTPOypfxtNaxXSQGHPWkodhvXv_g&usqp=CAU" alt="cow" />
         </div>
     )
 }
--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,33 @@ import React, { useEffect, useState } from "react";
 
 //this will display collage pictures of MooMoo's and navbar 
 function Home() {
-    // const [data, setData] = useState([]);
+    const [cow, setCowData] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:3000/cows")
-        .then(resp => resp.json())
-        .then((data) => console.log("json", data))
+        fetch("http://localhost:3001/cows")
+            .then(resp => resp.json())
+            .then((data) => {
+                console.log("json", data);
+                setCowData(data)
+            });
     }, []);
 
     return (
-        <div id="title">
-            <header>MooGram!</header>
+        <div>
+            <header id="title">MooGram!</header>
+                {cow.map((cows) => (
+                    <div key={cows.id}>
+                        <h3>{cows.username}</h3>
+                        <p>{cows.location}</p>
+                        <img src={cows.picture} alt="cow" />
+                        <p>{cows.date}</p>
+                        <p>{cows.caption}</p>
+                        <h3>Comments:</h3>
+                        <p>{cows.comment1}</p>
+                        <p>{cows.comments2}</p>
+                    </div>
+                )
+                )}
         </div>
 
     );
